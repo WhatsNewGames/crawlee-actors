a685e997a0b8a7057a6b8977def10c049905771a
Joël Charles <joel.charles91@gmail.com>
2022-09-15 17:52:03 +0200

    chore: fix build step

---
+++ apify-push.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+folder="$1"
+script_dir=$(dirname $(realpath "$0"))
+
+echo folder: $folder
+
+pushd $script_dir
+
+ln -s -f "$script_dir/packages/$folder/apify.json" "$script_dir/apify.json"
+ln -s -f "$script_dir/packages/$folder/INPUT_SCHEMA.json" "$script_dir/INPUT_SCHEMA.json"
+
+apify push
+
+rm -f "$script_dir/apify.json"
+rm -f "$script_dir/INPUT_SCHEMA.json"
+
+popd
