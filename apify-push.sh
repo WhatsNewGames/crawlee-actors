68821c5ead5b0e14f0813b4e09379210fc301ac8
Joël Charles <joel.charles91@gmail.com>
2022-09-20 18:14:31 +0200

    fix: image build scripts

--- apify-push.sh
+++ apify-push.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
-folder="$1"
+FOLDER="$1"
 script_dir=$(dirname $(realpath "$0"))
 
-# TODO: check that $folder exists and corresponds to pwd
+# TODO: check that $FOLDER exists and corresponds to pwd
 
-echo folder: $folder
+echo FOLDER: $FOLDER
 
 pushd $script_dir
 
-ln -s -f "$script_dir/packages/$folder/apify.json" "$script_dir/apify.json"
-ln -s -f "$script_dir/packages/$folder/INPUT_SCHEMA.json" "$script_dir/INPUT_SCHEMA.json"
+pwd
+
+ln -s -f "$script_dir/packages/$FOLDER/apify.json" "$script_dir/apify.json"
+ln -s -f "$script_dir/packages/$FOLDER/INPUT_SCHEMA.json" "$script_dir/INPUT_SCHEMA.json"
+
+sed "s/{{FOLDER}}/$FOLDER/g" "$script_dir/Dockerfile.template" > "$script_dir/Dockerfile"
+# If Dockerfile is in .gitignore, Docker command ignores it...
+sed -i "s/\/Dockerfile/\/.Dockerfile/" "$script_dir/.gitignore"
 
 apify push
 
+sed -i "s/\/\.Dockerfile/\/Dockerfile/" "$script_dir/.gitignore"
+rm -f "$script_dir/Dockerfile"
 rm -f "$script_dir/apify.json"
 rm -f "$script_dir/INPUT_SCHEMA.json"
 
