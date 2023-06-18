7f3098161a6ae4dac0a22b0685a74891f0216db2
Joël Charles <joel.charles91@gmail.com>
2023-06-18 18:58:33 +0200

    fix(diablo-iv): update parsing

--- packages/diablo-iv/src/routes.ts
+++ packages/diablo-iv/src/routes.ts
@@ -5,23 +5,17 @@ import { createHash } from 'crypto';
 export const router = createCheerioRouter();
 
 router.addDefaultHandler(async ({ request, $, log }) => {
-  let currentPoint = $('#article-detail .ArticleDetail-content').children().first().next();
-  currentPoint = currentPoint.nextAll('hr').first();
-  let stop = false;
+  const allDetails = $('#article-detail .ArticleDetail-content details');
 
-  while (!stop) {
-    const $post = currentPoint.nextUntil('hr');
+  const promises: Promise<unknown>[] = [];
 
-    const date = $post.first().text().trim();
-    const title = $post.nextAll('h4').first().text().trim().replace(/^- /, '');
+  allDetails.each(function () {
+    const titleAndDate = $('summary', this).text().trim();
+    const title = titleAndDate.replace(/ - (.*?)$/, '');
+    const date = titleAndDate.replace(/^.* - /, '');
     const url = request.url + '#' + createHash('sha256').update(title).digest('hex').substring(0, 12);
-    stop = title.includes('Build #42122');
-
-    const contentRange = stop
-      ? $post.nextAll('h4').first().nextUntil('p')
-      : $post.nextAll('h4').first().nextUntil('hr');
-
-    const content = htmlAll($, contentRange);
+    $('summary', this).remove();
+    const content = htmlAll($, $(this));
 
     if (!content || !date) {
       log.error('Page scraped but selector returned empty result', {
@@ -34,13 +28,52 @@ router.addDefaultHandler(async ({ request, $, log }) => {
 
     log.info('Page scraped', { url, title, date });
 
-    await pushData({
-      url,
-      date: { date, format: 'MMMM Do YYYY' },
-      title,
-      content,
-    });
+    promises.push(
+      pushData({
+        url,
+        date: { date, format: 'MMMM Do YYYY' },
+        title,
+        content,
+      }),
+    );
+  });
 
-    currentPoint = currentPoint.nextAll('hr').first();
-  }
+  await Promise.all(promises);
+  // currentPoint = currentPoint.nextAll('hr').first();
+  // let stop = false;
+
+  // while (!stop) {
+  //   const $post = currentPoint.nextUntil('hr');
+
+  //   const date = $post.first().text().trim();
+  //   const title = $post.nextAll('h4').first().text().trim().replace(/^- /, '');
+  //   const url = request.url + '#' + createHash('sha256').update(title).digest('hex').substring(0, 12);
+  //   stop = title.includes('Build #42122');
+
+  //   const contentRange = stop
+  //     ? $post.nextAll('h4').first().nextUntil('p')
+  //     : $post.nextAll('h4').first().nextUntil('hr');
+
+  //   const content = htmlAll($, contentRange);
+
+  // if (!content || !date) {
+  //   log.error('Page scraped but selector returned empty result', {
+  //     date,
+  //     url,
+  //     title,
+  //   });
+  //   return;
+  // }
+
+  // log.info('Page scraped', { url, title, date });
+
+  // await pushData({
+  //   url,
+  //   date: { date, format: 'MMMM Do YYYY' },
+  //   title,
+  //   content,
+  // });
+
+  //   currentPoint = currentPoint.nextAll('hr').first();
+  // }
 });
